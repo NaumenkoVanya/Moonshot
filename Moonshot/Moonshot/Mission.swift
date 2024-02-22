5764be6c684483d646b33d59fb838e058cc20f92
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-22 23:00:34 +0300

    Update ContentView

--- Moonshot/Moonshot/Mission.swift
+++ Moonshot/Moonshot/Mission.swift
@@ -18,4 +18,14 @@ struct Mission: Codable, Identifiable {
     let crew: [CrewRole]
     let description: String
 
+    var displayName: String {
+        "Apollo\(id)"
+    }
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
 }
